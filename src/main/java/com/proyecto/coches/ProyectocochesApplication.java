19f904df179d2dd11cd3c7d1deee8b6d7e072630
Gustavo Echenique <gustavo.echenique@live.com.ar>
2024-09-04 21:49:55 -0300

    Comienzo de proyecto

---
+++ src/main/java/com/proyecto/coches/ProyectocochesApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectocochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectocochesApplication.class, args);
+	}
+
+}
